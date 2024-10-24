0534319404e14252aa6fed4dc697eb2214a8a102
Anny <104777994+Any09@users.noreply.github.com>
2024-10-24 15:13:51 -0300

    ccu

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -22,7 +22,7 @@ async function quantidadeUsuariosPorRede() {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Redes sociais com mais usuários no mundo',
+            text: 'Nomes mais comuns mais comuns no Brasil',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
