fca69594de9eeb14dd28d751125718bb02dc97fc
Anny <104777994+Any09@users.noreply.github.com>
2024-10-31 15:22:32 -0300

    fdfdf

---
+++ graficos/gr-3.js
@@ -0,0 +1,57 @@
+import { getCSS, tickConfig, criarGrafico } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = ['Maria','José','Ana','João','Antonio','Francisco','Francisca','Marcia','Pedro','Adriana']
+    const quantidadeDeUsuarios = ['11734129','5754529','3089858','2984119','2576348','1772197','721637','551855','1219605','565621']
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Nomes mais comuns mais comuns no Brasil:',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'nomes mais comuns no Brasil em 2010',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Quantidade de pessoas com esse nome',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+
+    }
+
+    criarGrafico(data, layout)
+}
+
+quantidadeUsuariosPorRede()
