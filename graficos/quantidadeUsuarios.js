8f49c10066e03e1e4c323b02c59ce641e5865725
Anny <104777994+Any09@users.noreply.github.com>
2024-11-07 14:50:36 -0300

    dfghjjk

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -22,7 +22,7 @@ async function quantidadeUsuariosPorRede() {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Nomes mais comuns mais comuns no Brasil:',
+            text: 'Nomes mais comuns no Brasil:',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
