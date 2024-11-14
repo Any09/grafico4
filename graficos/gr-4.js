b9ff8d0aecbdeae15db137aab3e17a2b0a7151f0
Anny <104777994+Any09@users.noreply.github.com>
2024-11-14 13:37:32 -0300

    jhjhj

--- graficos/gr-4.js
+++ graficos/gr-4.js
@@ -1,28 +1,43 @@
-import { getCSS, tickConfig, criarGrafico } from "./common.js"
+import { criarGrafico, getCSS, incluirTexto } from "./common.js"
 
-async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = ['Miguel','Arthur', 'Heitor','Maria Alice','Helena','Gael','Valentina','Maite','Laura','Bernardo']
-    const quantidadeDeUsuarios = ['26941','23440','20694','24019','22202','25252','9987','11183','16953','15659']
+async function redesSociaisFavoritasMinhaEscola() {
+    const dadosLocaisString = localStorage.getItem('respostaRedesSociais')
+    if (dadosLocaisString) {
+        const dadosLocais = JSON.parse(dadosLocaisString)
+        processarDados(dadosLocais)
+    } else {
+        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+        const res = await fetch(url)
+        const dados = await res.json()
+        localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
+        processarDados(dados)
+    }
+}
+
+function processarDados(dados) {
+    const redesSociais = dados.slice(1).map(redes => redes[1])
+    const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
+        acc[redesSociais] = (acc[redesSociais] || 0) + 1
+        return acc
+    }, {})
+    const valores = ['11.4','9.3','8.2','7.4','6.5','6.1','5.8','5.5','5.2','4.9']
+    const labels = ['Silva','Santos','Oliveira','Souza','Rocha','Pereira','Martins','Costa','Alves','Dias']
 
     const data = [
         {
-            x: nomeDasRedes, 
-            y: quantidadeDeUsuarios, 
+            values: valores,
+            labels: labels,
             type: 'pie',
-            marker: {
-                color: getCSS('--primary-color')
-            }
+            textinfo: 'label+percent'
         }
     ]
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
+        height: 700,
         title: {
-            text: 'Nomes mais comuns no Brasil:',
+            text: 'Sobrenomes mais comuns no Brasil de acordo com pesquisas(IBGE).',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
@@ -30,28 +45,16 @@ async function quantidadeUsuariosPorRede() {
                 size: 30
             }
         },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Nomes mais comuns no Brasil atualmente',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Quantidade de pessoas com esse nome',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
+        legend: {
+            font: {
+                color: getCSS('--primary-color'),
+                size: 16
             }
         }
-
     }
 
     criarGrafico(data, layout)
+    incluirTexto(`No Brasil o sobrenome <span>Silva</span> é o mais comum, conforme as pesquisas feitas por nós.`)
 }
 
-quantidadeUsuariosPorRede()
+redesSociaisFavoritasMinhaEscola()
